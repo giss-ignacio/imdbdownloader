2221c33eb97775b3d2862f801cf4021df3770ec5
giss-ignacio <giss.ignacio@gmail.com>
2016-08-18 04:44:11 -0300

    adding categories

--- main.rb
+++ main.rb
@@ -15,9 +15,39 @@ def regex_form(category)
 
 end
 
+def add_movies	
+	movies_reg = regex_form("movies")
+		
+	match_prev = ['', '', '', '', '']
+	series = ""
+
+	datacom = data.prepare("INSERT INTO Movies (title, year, is_series) VALUES (?, ?, ?);")
+	i = 0
+	data.transaction do
+		$data.execute "DELETE FROM Movies;"
+	
+		File.new("data/movies.list").each_line do |l|
+			if match = movies_reg.match(l)			
+				unless match[match.length - 1].nil?
+				series = match[1]
+				datacom.execute!(match[1].tr(',".',''), match[match.length - 2][0..3].to_i, 1)			
+				else 
+				if ((match[1] != match_prev[1]) || (match[match.length - 2] != match_prev[match_prev.length - 2])) && (match[1] != series)
+					datacom.execute!(match[1].tr(',".',''), match[match.length - 2][0..3].to_i, 0)
+				end
+				end
+			end
+		end
+	end
+	
+	puts
+end
+
 def add_all
+	data = SQLite3::Database.new( "movies.sqlite3" )
+	
 	puts "Processing movies database"
-	add_movies	
+	add_movies(data)
 	
 	add_times
 	
